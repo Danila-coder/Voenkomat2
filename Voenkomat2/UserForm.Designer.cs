e16bb556de70723bb58e6dd8a0abc56164b874be
Данила <danil.didenko.2016@mail.ru>
2025-01-23 02:50:35 +0300

    Реализована возможность создания записей Граждан. Поиск по параметрам

--- Voenkomat2/UserForm.Designer.cs
+++ Voenkomat2/UserForm.Designer.cs
@@ -29,6 +29,25 @@
         private void InitializeComponent()
         {
             dataGridView1 = new DataGridView();
+            buttonRecord = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            dateTimePicker1 = new DateTimePicker();
+            textBox4 = new TextBox();
+            comboBox1 = new ComboBox();
+            comboBox2 = new ComboBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            textBoxSearch = new TextBox();
+            label8 = new Label();
+            buttonSearch = new Button();
+            buttonClearFilter = new Button();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
@@ -39,23 +58,222 @@
             dataGridView1.Location = new Point(12, 12);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.RowHeadersWidth = 51;
-            dataGridView1.Size = new Size(945, 188);
+            dataGridView1.Size = new Size(945, 212);
             dataGridView1.TabIndex = 0;
+            // 
+            // buttonRecord
+            // 
+            buttonRecord.Location = new Point(235, 411);
+            buttonRecord.Name = "buttonRecord";
+            buttonRecord.Size = new Size(94, 29);
+            buttonRecord.TabIndex = 1;
+            buttonRecord.Text = "Создать";
+            buttonRecord.UseVisualStyleBackColor = true;
+            buttonRecord.Click += buttonRecord_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 251);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(182, 27);
+            textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(12, 304);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(182, 27);
+            textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(12, 357);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(182, 27);
+            textBox3.TabIndex = 4;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(12, 410);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(182, 27);
+            dateTimePicker1.TabIndex = 5;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(235, 304);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(182, 27);
+            textBox4.TabIndex = 6;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(235, 250);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(182, 28);
+            comboBox1.TabIndex = 7;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(235, 357);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(182, 28);
+            comboBox2.TabIndex = 8;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 228);
+            label1.Name = "label1";
+            label1.Size = new Size(73, 20);
+            label1.TabIndex = 9;
+            label1.Text = "Фамилия";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 281);
+            label2.Name = "label2";
+            label2.Size = new Size(39, 20);
+            label2.TabIndex = 10;
+            label2.Text = "Имя";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 334);
+            label3.Name = "label3";
+            label3.Size = new Size(72, 20);
+            label3.TabIndex = 11;
+            label3.Text = "Отчество";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(12, 387);
+            label4.Name = "label4";
+            label4.Size = new Size(116, 20);
+            label4.TabIndex = 12;
+            label4.Text = "Дата рождения";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(235, 228);
+            label5.Name = "label5";
+            label5.Size = new Size(104, 20);
+            label5.TabIndex = 13;
+            label5.Text = "Образование";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(235, 281);
+            label6.Name = "label6";
+            label6.Size = new Size(51, 20);
+            label6.TabIndex = 14;
+            label6.Text = "Адрес";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(235, 334);
+            label7.Name = "label7";
+            label7.Size = new Size(52, 20);
+            label7.TabIndex = 15;
+            label7.Text = "Статус";
+            // 
+            // textBoxSearch
+            // 
+            textBoxSearch.Location = new Point(569, 251);
+            textBoxSearch.Name = "textBoxSearch";
+            textBoxSearch.Size = new Size(388, 27);
+            textBoxSearch.TabIndex = 16;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(569, 228);
+            label8.Name = "label8";
+            label8.Size = new Size(188, 20);
+            label8.TabIndex = 17;
+            label8.Text = "Введите критерий поиска";
+            // 
+            // buttonSearch
+            // 
+            buttonSearch.Location = new Point(569, 284);
+            buttonSearch.Name = "buttonSearch";
+            buttonSearch.Size = new Size(94, 29);
+            buttonSearch.TabIndex = 18;
+            buttonSearch.Text = "Поиск";
+            buttonSearch.UseVisualStyleBackColor = true;
+            buttonSearch.Click += buttonSearch_Click;
+            // 
+            // buttonClearFilter
+            // 
+            buttonClearFilter.Location = new Point(669, 284);
+            buttonClearFilter.Name = "buttonClearFilter";
+            buttonClearFilter.Size = new Size(94, 29);
+            buttonClearFilter.TabIndex = 19;
+            buttonClearFilter.Text = "Очистить";
+            buttonClearFilter.UseVisualStyleBackColor = true;
+            buttonClearFilter.Click += buttonClearFilter_Click;
             // 
             // UserForm
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(969, 450);
+            ClientSize = new Size(969, 498);
+            Controls.Add(buttonClearFilter);
+            Controls.Add(buttonSearch);
+            Controls.Add(label8);
+            Controls.Add(textBoxSearch);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(comboBox2);
+            Controls.Add(comboBox1);
+            Controls.Add(textBox4);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(buttonRecord);
             Controls.Add(dataGridView1);
             Name = "UserForm";
             Text = "UserForm";
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private DataGridView dataGridView1;
+        private Button buttonRecord;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private DateTimePicker dateTimePicker1;
+        private TextBox textBox4;
+        private ComboBox comboBox1;
+        private ComboBox comboBox2;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private TextBox textBoxSearch;
+        private Label label8;
+        private Button buttonSearch;
+        private Button buttonClearFilter;
     }
 }
--- Voenkomat2/UserForm.cs
+++ Voenkomat2/UserForm.cs
@@ -23,6 +23,8 @@ namespace Voenkomat2
             LoadData();
             SubscribeToRowPostPaint();
 
+
+            FillComboBoxes();
         }
 
         private void ConnectToDatabase()
@@ -47,8 +49,6 @@ namespace Voenkomat2
                 // SQL запрос для получения данных, исключая поле ID
                 string query = "SELECT Фамилия, Имя, Отчество, [Дата рождения], Образование, Адрес, Статус FROM Гражданин";
                 SQLiteCommand command = new SQLiteCommand(query, sqliteConn);
-
-                // Чтение данных
                 SQLiteDataAdapter dataAdapter = new SQLiteDataAdapter(command);
                 DataTable dataTable = new DataTable();
                 dataAdapter.Fill(dataTable);
@@ -59,6 +59,86 @@ namespace Voenkomat2
             catch (Exception ex)
             {
                 MessageBox.Show("Ошибка при загрузке данных: " + ex.Message);
+            }
+        }
+
+        // Заполнение ComboBox значениями
+        private void FillComboBoxes()
+        {
+            // Добавление значений в ComboBox для образования
+            comboBox1.Items.Add("Высшее");
+            comboBox1.Items.Add("Среднее");
+            comboBox1.Items.Add("Незаконченное высшее");
+            comboBox1.Items.Add("Среднее специальное");
+
+            // Выбор первого элемента по умолчанию (если нужно)
+            comboBox1.SelectedIndex = 0;
+
+            // Добавление значений в ComboBox для статуса
+            comboBox2.Items.Add("Служит");
+            comboBox2.Items.Add("Пенсионер");
+            comboBox2.Items.Add("Студент");
+
+            // Выбор первого элемента по умолчанию (если нужно)
+            comboBox2.SelectedIndex = 0;
+        }
+
+        private void buttonRecord_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Получаем данные из текстовых полей, DateTimePicker и ComboBox
+                string lastName = textBox1.Text;  // Фамилия
+                string firstName = textBox2.Text; // Имя
+                string middleName = textBox3.Text; // Отчество
+                DateTime birthDate = dateTimePicker1.Value; // Дата рождения
+                string education = comboBox1.SelectedItem?.ToString(); // Образование (выбор из ComboBox)
+                string address = textBox4.Text; // Адрес (ввод в TextBox)
+                string status = comboBox2.SelectedItem?.ToString(); // Статус (выбор из ComboBox)
+
+                // Проверка, чтобы не вставлять пустые значения из ComboBox
+                if (string.IsNullOrEmpty(education) || string.IsNullOrEmpty(status))
+                {
+                    MessageBox.Show("Выберите значение в обоих ComboBox.");
+                    return;
+                }
+
+                // SQL запрос для вставки данных
+                string query = "INSERT INTO Гражданин (Фамилия, Имя, Отчество, [Дата рождения], Образование, Адрес, Статус) " +
+                               "VALUES (@lastName, @firstName, @middleName, @birthDate, @education, @address, @status)";
+
+                // Создаем команду
+                SQLiteCommand command = new SQLiteCommand(query, sqliteConn);
+                command.Parameters.AddWithValue("@lastName", lastName);
+                command.Parameters.AddWithValue("@firstName", firstName);
+                command.Parameters.AddWithValue("@middleName", middleName);
+                command.Parameters.AddWithValue("@birthDate", birthDate.ToString("yyyy-MM-dd"));
+                command.Parameters.AddWithValue("@education", education);
+                command.Parameters.AddWithValue("@address", address);
+                command.Parameters.AddWithValue("@status", status);
+
+                // Выполняем команду для вставки данных
+                command.ExecuteNonQuery();
+
+                // После добавления, перезагружаем данные в DataGridView
+                LoadData();
+
+                // Очистить текстовые поля после добавления
+                textBox1.Clear();
+                textBox2.Clear();
+                textBox3.Clear();
+                textBox4.Clear(); // Очистить поле ввода адреса
+
+                // Установить индекс на первый элемент в ComboBox после добавления записи
+                comboBox1.SelectedIndex = 0; // Установить первый элемент (например, "Высшее")
+                comboBox2.SelectedIndex = 0; // Установить первый элемент (например, "Служит")
+
+                // Сбросить дату на текущую
+                dateTimePicker1.Value = DateTime.Now;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка при добавлении записи: " + ex.Message);
             }
         }
 
@@ -89,6 +169,90 @@ namespace Voenkomat2
                 }
             }
         }
-    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+            string searchQuery = textBoxSearch.Text.Trim(); // Получаем текст из поля поиска
+
+            if (string.IsNullOrEmpty(searchQuery))
+            {
+                MessageBox.Show("Пожалуйста, заполните поле поиска.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                // Выполнить поиск
+                SearchData(searchQuery);
+            }
+        }
+
+        private void buttonClearFilter_Click(object sender, EventArgs e)
+        {
+            // Загружаем все данные без фильтрации
+            LoadData();
+        }
+
+        private void SearchData(string searchQuery)
+        {
+            try
+            {
+                // Разделяем поисковый запрос на отдельные слова
+                string[] searchTerms = searchQuery.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                // Строим SQL запрос с операторами LIKE
+                string query = "SELECT Фамилия, Имя, Отчество, [Дата рождения], Образование, Адрес, Статус FROM Гражданин WHERE ";
+
+                for (int i = 0; i < searchTerms.Length; i++)
+                {
+                    string searchTerm = "%" + searchTerms[i] + "%"; // Используем % для LIKE
+
+                    if (i > 0)
+                    {
+                        query += " AND "; // Добавляем логический оператор AND
+                    }
+
+                    // Добавляем условие поиска для каждого поля
+                    query += "(Фамилия LIKE @SearchTerm" + i + " OR Имя LIKE @SearchTerm" + i + " OR Отчество LIKE @SearchTerm" + i + " ";
+                    query += "OR Образование LIKE @SearchTerm" + i + " OR Статус LIKE @SearchTerm" + i + ")";
+                }
+
+                SQLiteCommand command = new SQLiteCommand(query, sqliteConn);
+
+                // Добавляем параметры для защиты от SQL инъекций
+                for (int i = 0; i < searchTerms.Length; i++)
+                {
+                    command.Parameters.AddWithValue("@SearchTerm" + i, "%" + searchTerms[i] + "%");
+                }
+
+                SQLiteDataAdapter dataAdapter = new SQLiteDataAdapter(command);
+                DataTable dataTable = new DataTable();
+                dataAdapter.Fill(dataTable);
+
+                // Привязываем результат к DataGridView
+                dataGridView1.DataSource = dataTable;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка при поиске данных: " + ex.Message);
+            }
+        }
+
+
     }
 }
